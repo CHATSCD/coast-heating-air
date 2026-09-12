1d6d2610cdb2aa98497837ec43c968d0f206a8f3
CHATSCD <shaundubuisson24@gmail.com>
2026-09-12 06:08:21 -0500

    AI update

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+import { siteUrl } from "@/lib/site";
+
+export default function sitemap() {
+  return [
+    {
+      url: siteUrl(),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
